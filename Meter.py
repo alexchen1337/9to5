ff8147fb5e998968ef466b8faf638348f0e13983
ethandietrich00808 <edietri2@vols.utk.edu>
2024-09-30 11:05:11 -0400

    Update Meter.py
    Get, Set, Warning, and History fxns added to Meter.py

--- Meter.py
+++ Meter.py
@@ -1,14 +1,19 @@
+# Meters for Health, Energy, Family Happiness
+
 class Meter:
     def __init__(self, name, max_value, min_value=0):
         self.name = name
         self.value = max_value
         self.max_value = max_value
         self.min_value = min_value
+        self.history = []
 
     def increase(self, amount):
+        self.history.append(self.value)
         self.value = min(self.max_value, self.value + amount)
 
     def decrease(self, amount):
+        self.history.append(self.value)
         self.value = max(self.min_value, self.value - amount)
 
     def is_depleted(self):
@@ -16,3 +21,23 @@ class Meter:
 
     def __str__(self):
         return f"{self.name}: {self.value}/{self.max_value}"
+
+    # returns all 30 days of meter levels at end of game
+    def get_history(self):
+        return self.history
+    
+    def get_value(self):
+        return self.value
+    
+    def set_value(self, change):
+        if change > 0:
+            self.increase(change)
+        else:
+            self.decrease(-change)
+    
+    # output warning when meter level is too low
+    def level_warning(self):
+        if self.value < .2 * self.max_value:
+            print(f"Warning: {self.name} is low!")
+
+    
